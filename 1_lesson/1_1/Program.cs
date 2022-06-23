324e31f504af80f76ff754bd78760fbd4a820ee2
thanatos_inf <thanatos_inf@mail.ru>
2022-06-23 11:12:34 +0300

    1 task

--- 1_lesson/1_1/Program.cs
+++ 1_lesson/1_1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿//string num_str = Console.ReadLine();
+//int num = int.Parse(num_str);
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+if(numA * numA == numB)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
+
+
