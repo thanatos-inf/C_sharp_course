58746d039ecd9a541cc0031c74a8be7fddb9c274
thanatos_inf <thanatos_inf@mail.ru>
2022-06-28 15:08:16 +0300

    task 3

---
+++ HW1/3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num % 2 == 0) 
+{
+    Console.Write("Введённое Вами число является чётным!");
+}
+else
+{
+    Console.Write("Введённое Вами число НЕ является чётным!");
+}
