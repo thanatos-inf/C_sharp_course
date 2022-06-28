f2739abc84d2e874dbdcdc19ace767340b94c74e
thanatos_inf <thanatos_inf@mail.ru>
2022-06-28 14:45:07 +0300

    task 2

---
+++ HW1/2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите три числа: ");
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+int numC = int.Parse(Console.ReadLine());
+int max = numA;
+if(numB > max) max = numB;
+if(numC > max) max = numC;
+
+Console.Write("Максимальное число: ");
+Console.Write(max);
