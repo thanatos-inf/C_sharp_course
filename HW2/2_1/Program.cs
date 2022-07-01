eeb4caf99dfe49a2d299384224410453239b8647
thanatos_inf <thanatos_inf@mail.ru>
2022-07-01 14:05:57 +0300

    1st task

---
+++ HW2/2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+
+
+int SecondNum(int num)
+{
+    return (num % 100) / 10;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Вторая цифра введённого числа: ");
+Console.Write(SecondNum(num));
