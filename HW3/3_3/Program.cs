ff62c30384890329b1acc206f8589c49d57f55ce
thanatos_inf <thanatos_inf@mail.ru>
2022-07-03 19:14:10 +0300

    HW3 task 2 and 3

---
+++ HW3/3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cubes(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.Write($"{Math.Pow(count, 3)} ");
+        count++;
+    }
+}
+Cubes(5);
