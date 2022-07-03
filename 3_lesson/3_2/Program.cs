d61bf164b5df4660b6027d0a4fc3916da9ad9d24
thanatos_inf <thanatos_inf@mail.ru>
2022-07-03 19:13:17 +0300

    3_2 cor

--- 3_lesson/3_2/Program.cs
+++ 3_lesson/3_2/Program.cs
@@ -6,4 +6,5 @@ double Roots(double x1, double x2, double y1, double y2)
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 }
 
-Console.WriteLine(Roots(3, 6, 2, 1));
+Console.WriteLine(Roots(3, 2, 6, 1));
+Console.WriteLine(Roots(7, 1, -5, -1));
