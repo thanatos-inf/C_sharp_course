c0a7862117c6d8fd6025e3e522e18d155859321f
thanatos_inf <thanatos_inf@mail.ru>
2022-07-07 11:47:09 +0300

    task 5_4 by DmitryR

---
+++ 5_lesson/5_5/Program.cs
@@ -0,0 +1,46 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+int[] CreateNewArray(int arrayLength)
+{
+    return new int[arrayLength];
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] ProductOfPairsOfNumbersInArray(int[] originalArray)
+{
+    int oLength = originalArray.Length;
+    int[] result = new int[oLength / 2 + oLength % 2];
+    for (int i = 0; i < (oLength / 2); i++)
+    {
+        result[i] = originalArray[i] * originalArray[oLength - i - 1];
+    }
+
+    if (oLength % 2 != 0)
+    {
+        result[oLength / 2] = originalArray[oLength / 2];
+    }
+
+    return result;
+}
+
+int[] originalArray = CreateNewArray(6);
+FillArray(originalArray);
+PrintArray(originalArray);
+Console.Write("-> ");
+PrintArray(ProductOfPairsOfNumbersInArray(originalArray));
