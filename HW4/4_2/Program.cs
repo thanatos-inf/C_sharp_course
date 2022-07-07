aee17096078bdc4ea372858ca0aa0f4a3a21c632
thanatos_inf <thanatos_inf@mail.ru>
2022-07-07 15:49:50 +0300

    4_2 modified

--- HW4/4_2/Program.cs
+++ HW4/4_2/Program.cs
@@ -16,21 +16,3 @@ Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
 Console.Write("Сумма цифр введённого Вами числа равна: ");
 Console.Write(NumSum(num));
-
-//     // string result = num.ToString();
-//     // var ints = result.Split(' ').Select(Int32.Parse).ToArray();
-
-// // char NumSum(char num)
-// // {
-// //     char[] arr = num.ToString().ToCharArray();
-// //     char count = arr.Length;
-// //     char sum = 0;
-
-// //     for(char i = 1; i <= count; i++)
-// //     {
-// //         sum = sum + arr[i];
-// //     } 
-// //     return sum;
-// // }
-
-// // Console.WriteLine(NumSum(12345));
