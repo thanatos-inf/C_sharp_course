4430a1a54101a225434bcaa52c3a3686bf66657a
thanatos_inf <thanatos_inf@mail.ru>
2022-07-07 10:47:38 +0300

    demo + 5_1

---
+++ 5_lesson/5_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// DEMO
+// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{       
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);        
+    }    
+    return arr;
+}
+
+void SumMaxMin(int[] arr)
+{
+    int s_max, s_min;
+    s_max = s_min = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >=0) s_max +=arr[i];
+        else s_min += arr[i];
+    }
+    Console.WriteLine($"sum max: {s_max}, sum min: {s_min}");
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+SumMaxMin(arr_1);
---
+++ 5_lesson/5_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие
+// отрицательные, и наоборот.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{       
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);        
+    }    
+    return arr;
+}
+
+void Change(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        // if (arr[i] > 0) arr[i] = -arr[i];
+        // else arr[i] = -arr[i];
+        arr[i] = -arr[i];
+    }
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+Change(arr_1);
+Print(arr_1);
