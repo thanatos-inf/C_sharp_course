f27cacb7548667886e30b405f4d056159e7ab4c5
thanatos_inf <thanatos_inf@mail.ru>
2022-07-07 16:36:27 +0300

    4_3

---
+++ HW4/4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// случайными числами и выводит их на экран.
+
+int[] fillArray(int size)
+{       
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);        
+    }    
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] new_array = fillArray(8);
+PrintArray(new_array);
