cb941b8b0b531c25596e7b47d6982c548be32485
thanatos_inf <thanatos_inf@mail.ru>
2022-07-07 18:23:32 +0300

    5_2

--- HW5/5_2/Program.cs
+++ HW5/5_2/Program.cs
@@ -1,3 +1,39 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих 
 // на нечётных позициях.
 
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+int SumNum(int[] arr)
+{
+    int oddSum = 0;
+
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        oddSum = oddSum + arr[i];
+    }
+    return oddSum;
+}
+
+int[] arr_1 = MassNums(6);
+Print(arr_1);
+Console.WriteLine(SumNum(arr_1));
