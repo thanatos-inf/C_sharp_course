36fe4b8a90c58e5cb3d8345cf7ac4b3fe47d45b0
thanatos_inf <thanatos_inf@mail.ru>
2022-07-11 11:08:27 +0300

    6_1 and 6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+void Binary(int number)
+{
+    string Dec = "";
+    while(number > 0)
+    {
+        Dec = number % 2 + Dec;
+        number /= 2;
+    }
+    Console.WriteLine(Dec);
+}
+
+Binary(45);
