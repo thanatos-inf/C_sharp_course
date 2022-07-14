3d30eb58caa88aac002b3437e606fb34d1b76f2d
thanatos_inf <thanatos_inf@mail.ru>
2022-07-14 10:58:03 +0300

    task 2

---
+++ 7_lesson/7_4/Program.cs
@@ -0,0 +1,53 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        } 
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {   
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }        
+    }
+    return arr;
+}
+
+int DiagSum ( int[,] Array)
+{
+    int rows = Array.GetLength(0);
+    int sum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        sum += Array[i, i];
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 1, 10);
+Print(arr_1);
+Console.WriteLine(DiagSum(arr_1));
