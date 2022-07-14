42702d69b527b06035664cc48aedeb0f14271d01
thanatos_inf <thanatos_inf@mail.ru>
2022-07-14 17:57:31 +0300

    it's done

---
+++ HW6/6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int CountPos (int num)
+{
+    int count = 0;
+    
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Число № {i}: ");
+        int a = int.Parse(Console.ReadLine());
+        if( a > 0 ) count++; 
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше нуля: {CountPos(a)}");
---
+++ HW6/6_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая будет создавать копию заданного двумерного массива 
+// с помощью поэлементного копирования.
+
+
+void PrintMatrix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] FillMatrix(int row, int column)
+{
+    double[,] matr = new double[row, column];
+    Random num = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matr[i, j] = Math.Round(num.NextDouble() * (100 + 101) - 100, 2);
+        }
+    }
+    return matr;
+}
+
+double[,] CopyMatrix(double[,] matr)
+{
+    double[,] new_matr = new double[matr.GetLength(0), matr.GetLength(1)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            new_matr[i, j] = matr [i, j];
+        }
+    }
+    return new_matr;
+}
+
+double[,] matr_1 = FillMatrix(3, 3);
+PrintMatrix(matr_1);
+double[,] matr_1_copy = CopyMatrix(matr_1);
+PrintMatrix(matr_1_copy);
