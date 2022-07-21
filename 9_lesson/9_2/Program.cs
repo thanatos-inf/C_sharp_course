ac50b0b6cedaed0facbe35ef06399327c365a4a8
thanatos_inf <thanatos_inf@mail.ru>
2022-07-21 10:59:38 +0300

    9_2 and 9_3

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void ValuesSet(int M, int N)
+{
+    if (N <  M) return;
+    ValuesSet(M, N-1);
+    Console.Write($"{N}, ");
+}
+
+ValuesSet(4, 8);
+
+// Console.Write("Введите число M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// void Sequence(int M, int N)
+// {
+//     if (N < M) return;
+//     Sequence(M, N-1);
+//     Console.Write($"{N}, ");
+// }
+// Sequence(M, N);
---
+++ 9_lesson/9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int ValuesSum(int num)
+{
+    if (num == 0) return 0;
+    return ValuesSum(num / 10) + num % 10;
+}
+
+Console.WriteLine(ValuesSum(1235));
